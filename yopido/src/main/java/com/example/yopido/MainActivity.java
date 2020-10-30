fba3dc8132e046bcc953e0b27ab553e7fa079fe0
ALEJANDRO IGLESIAS CALVO <UO247346@uniovi.es>
2020-10-30 17:28:15 +0100

    Click a los botones añadidos, falta implementar rla funcionalidad. Lanzan mensaje de aviso por ahora

--- yopido/src/main/java/com/example/yopido/MainActivity.java
+++ yopido/src/main/java/com/example/yopido/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.yopido;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnEntar;
+        Button btnRegistro;
+        Button btnPedirYa;
+        btnEntar = (Button) findViewById(R.id.btnEntrar);
+        btnEntar.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(findViewById(R.id.MainActivityLayaut), R.string.msg_Aun_NO_TOLAIIIIIIIII,
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        }));
+        btnRegistro = (Button) findViewById(R.id.btnRegistro);
+        btnRegistro.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(findViewById(R.id.MainActivityLayaut), R.string.msg_Aun_NO_TOLAIIIIIIIII,
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        }));
+        btnPedirYa = (Button) findViewById(R.id.btnPideYa);
+        btnPedirYa.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(findViewById(R.id.MainActivityLayaut), R.string.msg_Aun_NO_TOLAIIIIIIIII,
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        }));
     }
 }
