1d2745697b60638b5cda90794a4a7cfd792478da
Alejandro Iglesias Calvo <47303435+UO247346@users.noreply.github.com>
2021-01-03 23:45:23 +0100

    LLamadas a inciar sesion desde RealmDB.java

--- yopido/src/main/java/com/HUMMMM/yopido/datos/RealmDB.java
+++ yopido/src/main/java/com/HUMMMM/yopido/datos/RealmDB.java
@@ -1,5 +1,30 @@
 package com.HUMMMM.yopido.datos;
-import io.realm.Realm;
-public class RealmDB {
 
+import android.util.Log;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.User;
+
+public class RealmDB {
+    private static final String YOUR_APP_ID = "restavidbbdd-djyny";
+
+    public void InciarSesion(String email, String contrasena) {
+        String appID = YOUR_APP_ID; // replace this with your App ID
+        App app = new App(new AppConfiguration.Builder(appID)
+                .build());
+        Credentials emailPasswordCredentials = Credentials.emailPassword(email, contrasena);
+        AtomicReference<User> user = new AtomicReference<User>();
+        app.loginAsync(emailPasswordCredentials, it -> {
+            if (it.isSuccess()) {
+                Log.v("AUTH", "Successfully authenticated using an email and password.");
+                user.set(app.currentUser());
+            } else {
+                Log.e("AUTH", it.getError().toString());
+            }
+        });
+    }
 }
