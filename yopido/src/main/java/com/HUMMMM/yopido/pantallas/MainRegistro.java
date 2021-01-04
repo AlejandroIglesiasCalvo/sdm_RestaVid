e24dc045f69d94bc2035e44d2fac5be37a6a315a
carlos <uo237674@uniovi.es>
2021-01-04 19:30:03 +0100

    apellidos en registro de usuario y campos vacios comprobados.

--- yopido/src/main/java/com/HUMMMM/yopido/pantallas/MainRegistro.java
+++ yopido/src/main/java/com/HUMMMM/yopido/pantallas/MainRegistro.java
@@ -3,12 +3,15 @@ package com.HUMMMM.yopido.pantallas;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.HUMMMM.yopido.R;
+import com.HUMMMM.yopido.controlador.control.checks;
 import com.HUMMMM.yopido.controlador.navegacion.cambiarDeClase;
 import com.HUMMMM.yopido.pantallas.loguedUser.MainMenuLoggeado;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainRegistro extends BaseActivity {
 
@@ -16,14 +19,25 @@ public class MainRegistro extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        EditText correo =  findViewById(R.id.editTextCorreo);
+        EditText pass =  findViewById(R.id.editTextTextPassword);
+        EditText nombre =  findViewById(R.id.editTextTextNombre);
+        EditText apellidos =  findViewById(R.id.editTextTextApellidos);
+        EditText telefono =  findViewById(R.id.editTextPhone);
+
+
         Button btnRegistroAceptar;
-        Button btnRegistroCancelar;
 
         btnRegistroAceptar = (Button) findViewById(R.id.buttonRegistroAceptar);
         btnRegistroAceptar.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+            if(checks.camposRellenos(correo,pass,nombre,apellidos,telefono))
                 cambiarDeClase.MoverA(v.getContext(), MainMenuLoggeado.class);
+            else
+                Snackbar.make(findViewById(R.id.buttonRegistroAceptar), R.string.error_registro, Snackbar.LENGTH_SHORT).show();
             }
         }));
 
