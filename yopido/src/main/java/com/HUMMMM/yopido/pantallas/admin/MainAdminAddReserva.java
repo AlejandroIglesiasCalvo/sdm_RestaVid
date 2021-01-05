3907c3812294d552d78ce0ed2b6a3831f73e4c0b
carlos <uo237674@uniovi.es>
2021-01-05 10:12:09 +0100

    MainAdminAddReserva coger cosas del calendario, guardar cosas en reserva llamando a la consulta de la BDD. Y mensaje.

--- yopido/src/main/java/com/HUMMMM/yopido/pantallas/admin/MainAdminAddReserva.java
+++ yopido/src/main/java/com/HUMMMM/yopido/pantallas/admin/MainAdminAddReserva.java
@@ -3,25 +3,44 @@ package com.HUMMMM.yopido.pantallas.admin;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.HUMMMM.yopido.R;
 import com.HUMMMM.yopido.controlador.control.checks;
 import com.HUMMMM.yopido.controlador.navegacion.cambiarDeClase;
+import com.HUMMMM.yopido.datos.ReservaDataSource;
+import com.HUMMMM.yopido.modelo.Reserva;
 import com.HUMMMM.yopido.pantallas.BaseActivity;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.Date;
+
 public class MainAdminAddReserva extends BaseActivity {
+
+    final EditText nombre =  findViewById(R.id.editTextTextNombre);
+    final EditText telf =  findViewById(R.id.editTextPhone);
+    final Spinner numPersonas = findViewById(R.id.spPersonas);
+    final Spinner horaReserva = findViewById(R.id.spHora);
+
+    final CalendarView calendario = findViewById(R.id.calendarReserva);
+    Long date = calendario.getDate();
+
+    String diaSeleccionado;
+
+    String fechaseleccionada;
+    
+    ReservaDataSource rds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_reserva);
 
-        final EditText nombre =  findViewById(R.id.editTextTextNombre);
-        final EditText telf =  findViewById(R.id.editTextPhone);
+
         Button btnAddReserva;
 
         btnAddReserva = findViewById(R.id.button_Aceptar_Reserva_Admin);
@@ -36,14 +55,33 @@ public class MainAdminAddReserva extends BaseActivity {
                 else
                 {
                     //Se comprueban los datos.
-
                     // Se cambia finalmente, si sale bien, de clase
+                    comprobarDatos();
                     cambiarDeClase.MoverA(v.getContext(), MainActivityAdmin.class);
+
                 }
             }
         }));
+    }
 
+    private void comprobarDatos()
+    {
+        Reserva reserva = new Reserva();
+        reserva.setNombreUsuario(nombre.toString());
+        reserva.setTelef(telf.toString());
+        reserva.setNumeroDePersonas(Integer.parseInt(numPersonas.getSelectedItem().toString()));
+        reserva.setHora(horaReserva.toString());
 
-
+        calendario.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                if(calendario.getDate() != date){
+                    date = calendario.getDate();
+                    diaSeleccionado = dayOfMonth + "/" + month + "/" + year + "";
+                }
+            }
+        });
+        reserva.setFecha(fechaseleccionada);
+        
+        rds.createReserva(reserva);
     }
 }
